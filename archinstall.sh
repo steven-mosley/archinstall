fee4e9adbabe2aeba4693a653631798746602841
Steven Mosley <34114851+RetroSteve0@users.noreply.github.com>
2024-11-04 20:02:28 -0600

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -38,6 +38,7 @@ timedatectl set-ntp true
 dialog --title "Arch Linux Minimal Installer" --msgbox "Welcome to the Arch Linux Minimal Installer.\n\nThis installer provides a quick and easy minimal install for Arch Linux, setting up a base system that boots to a terminal." 12 70
 
 # Disk selection
+echo "[DEBUG] Prompting for disk selection"
 disk=$(dialog --stdout --title "Select Disk" --menu "Select the disk to install Arch Linux on:" 15 60 4 $(lsblk -dn -o NAME,SIZE | awk '{print "/dev/" $1 " " $2}'))
 if [ -z "$disk" ]; then
   dialog --msgbox "No disk selected. Exiting." 5 40
@@ -46,6 +47,7 @@ if [ -z "$disk" ]; then
 fi
 
 # Detect existing Windows partitions
+echo "[DEBUG] Detecting existing Windows partitions on $disk"
 disk_partitions=$(lsblk -o NAME,FSTYPE,LABEL,UUID,MOUNTPOINTS $disk | grep -E 'ntfs|FAT32')
 free_space=$(lsblk -b -o NAME,TYPE,FREE $disk | grep "disk" | awk '{print $3}')
 min_required_space=$((30 * 1024 * 1024 * 1024)) # Minimum required space is 30GB
@@ -76,6 +78,7 @@ if [ -n "$disk_partitions" ]; then
       fi
     else
       # Prompt user to select the Windows partition
+      echo "[DEBUG] Prompting for Windows partition to resize"
       windows_partition=$(dialog --stdout --title "Select Windows Partition" --menu "Select the main Windows partition to resize:" 15 60 4 $(lsblk -rn -o NAME,SIZE,FSTYPE $disk | grep ntfs | awk '{print $1 " " $2}'))
       if [ -z "$windows_partition" ]; then
         dialog --msgbox "No Windows partition selected. Exiting." 5 40
@@ -84,6 +87,7 @@ if [ -n "$disk_partitions" ]; then
       fi
       windows_partition="${disk}${windows_partition}"
       # Ask user how much space to allocate for Arch Linux
+      echo "[DEBUG] Prompting for space allocation for Arch Linux"
       arch_space=$(dialog --stdout --inputbox "Enter the amount of space (in GB) you want to allocate for Arch Linux:" 8 40)
       if [ -z "$arch_space" ]; then
         dialog --msgbox "No space entered. Exiting." 5 40
@@ -91,6 +95,7 @@ if [ -n "$disk_partitions" ]; then
         exit 1
       fi
       # Shrink Windows partition
+      echo "[DEBUG] Shrinking Windows partition $windows_partition by $arch_space GB"
       dialog --infobox "Shrinking Windows partition to create space for Arch Linux..." 7 50
       parted $disk resizepart ${windows_partition##*p} -${arch_space}G
     fi
@@ -107,9 +112,11 @@ else
     fi
   fi
   # Wipe the disk
+  echo "[DEBUG] Wiping the disk $disk"
   sgdisk --zap-all $disk
 
   # Create partitions
+  echo "[DEBUG] Creating partitions on $disk"
   # Partition 1: EFI System Partition
   sgdisk -n 1:0:+550M -t 1:ef00 $disk
   # Partition 2: Root partition
@@ -117,6 +124,7 @@ else
 fi
 
 # Get partition names
+echo "[DEBUG] Determining partition names"
 if [[ "$disk" == *"nvme"* ]]; then
   esp="${disk}p1"
   root_partition="${disk}p2"
@@ -126,14 +134,17 @@ else
 fi
 
 # Format partitions
+echo "[DEBUG] Formatting partitions"
 dialog --infobox "Formatting partitions..." 5 40
 mkfs.vfat -F32 $esp
 mkfs.btrfs -f -L Arch $root_partition
 
 # Mount root partition
+echo "[DEBUG] Mounting root partition $root_partition"
 mount $root_partition /mnt
 
 # Create Btrfs subvolumes
+echo "[DEBUG] Creating Btrfs subvolumes"
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
 btrfs su cr /mnt/@pkg
@@ -141,9 +152,11 @@ btrfs su cr /mnt/@log
 btrfs su cr /mnt/@snapshots
 
 # Unmount root partition
+echo "[DEBUG] Unmounting root partition"
 umount /mnt
 
 # Mount subvolumes with options
+echo "[DEBUG] Mounting Btrfs subvolumes"
 mount -o noatime,compress=zstd,discard=async,space_cache=v2,subvol=@ $root_partition /mnt
 mkdir -p /mnt/{boot/efi,home,var/cache/pacman/pkg,var/log,.snapshots}
 mount -o noatime,compress=zstd,discard=async,space_cache=v2,subvol=@home $root_partition /mnt/home
@@ -152,12 +165,14 @@ mount -o noatime,compress=zstd,discard=async,space_cache=v2,subvol=@log $root_pa
 mount -o noatime,compress=zstd,discard=async,space_cache=v2,subvol=@snapshots $root_partition /mnt/.snapshots
 
 # Mount EFI partition
+echo "[DEBUG] Mounting EFI partition $esp"
 if ! mount | grep -q '/mnt/boot/efi'; then
   mkdir -p /mnt/boot/efi
   mount $esp /mnt/boot/efi
 fi
 
 # Detect CPU and offer to install microcode
+echo "[DEBUG] Detecting CPU vendor"
 cpu_vendor=$(grep -m1 -E 'vendor_id|Vendor ID' /proc/cpuinfo | awk '{print $3}' | tr '[:upper:]' '[:lower:]')
 microcode_pkg=""
 microcode_img=""
@@ -179,6 +194,7 @@ else
 fi
 
 # Prompt for hostname
+echo "[DEBUG] Prompting for hostname"
 hostname=$(dialog --stdout --inputbox "Enter a hostname for your system:" 8 40)
 if [ -z "$hostname" ]; then
   dialog --msgbox "No hostname entered. Using default 'archlinux'." 6 50
@@ -186,6 +202,7 @@ if [ -z "$hostname" ]; then
 fi
 
 # Prompt for timezone
+echo "[DEBUG] Prompting for timezone"
 available_timezones=$(find /usr/share/zoneinfo/ -type f | sed 's#/usr/share/zoneinfo/##')
 timezone=$(dialog --stdout --title "Select Timezone" --menu "Select your timezone:" 20 60 15 $(echo "$available_timezones" | nl -w2 -s" "))
 if [ -z "$timezone" ]; then
@@ -194,6 +211,7 @@ if [ -z "$timezone" ]; then
 fi
 
 # Offer to install btrfs-progs
+echo "[DEBUG] Prompting for Btrfs tools installation"
 dialog --yesno "Would you like to install btrfs-progs for Btrfs management?" 7 60
 if [ $? -eq 0 ]; then
   btrfs_pkg="btrfs-progs"
@@ -202,6 +220,7 @@ else
 fi
 
 # Offer to install NetworkManager
+echo "[DEBUG] Prompting for NetworkManager installation"
 dialog --yesno "Would you like to install NetworkManager for network management?" 7 60
 if [ $? -eq 0 ]; then
   networkmanager_pkg="networkmanager"
@@ -210,6 +229,7 @@ else
 fi
 
 # Offer to enable ZRAM
+echo "[DEBUG] Prompting for ZRAM enablement"
 dialog --yesno "Would you like to enable ZRAM for swap?" 7 50
 if [ $? -eq 0 ]; then
   zram_pkg="zram-generator"
@@ -218,22 +238,28 @@ else
 fi
 
 # Install base system
+echo "[DEBUG] Installing base system"
 dialog --infobox "Installing base system..." 5 40
-pacstrap /mnt base linux linux-firmware $microcode_pkg $btrfs_pkg $zram_pkg $networkmanager_pkg
+pacstrap /mnt base linux linux-firmware $microcode_pkg "$btrfs_pkg" "$zram_pkg" "$networkmanager_pkg"
 
 # Generate fstab
+echo "[DEBUG] Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot into the new system
+echo "[DEBUG] Entering chroot to configure the new system"
 arch-chroot /mnt /bin/bash <<EOF
 # Set the timezone
+echo "[DEBUG] Setting timezone to $timezone"
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 hwclock --systohc
 
 # Set the hostname
+echo "[DEBUG] Setting hostname to $hostname"
 echo "$hostname" > /etc/hostname
 
 # Configure /etc/hosts
+echo "[DEBUG] Configuring /etc/hosts"
 cat <<EOL > /etc/hosts
 127.0.0.1   localhost
 ::1         localhost
@@ -241,6 +267,7 @@ cat <<EOL > /etc/hosts
 EOL
 
 # Generate locales
+echo "[DEBUG] Prompting for locale selection"
 available_locales=$(awk '/^[a-z]/ {print $1}' /usr/share/i18n/SUPPORTED)
 selected_locale=$(dialog --stdout --title "Select Locale" --menu "Select your locale:" 20 60 15 $(echo "$available_locales" | nl -w2 -s" "))
 if [ -z "$selected_locale" ]; then
@@ -252,6 +279,7 @@ locale-gen
 echo "LANG=$selected_locale" > /etc/locale.conf
 
 # Configure ZRAM if enabled
+echo "[DEBUG] Configuring ZRAM if enabled"
 if [ -n "$zram_pkg" ]; then
   cat <<EOM > /etc/systemd/zram-generator.conf
 [zram0]
@@ -260,46 +288,21 @@ compression-algorithm = zstd
 EOM
 fi
 
-EOF
-# Set the timezone
-ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
-hwclock --systohc
-
-# Set the hostname
-echo "$hostname" > /etc/hostname
-
-# Configure /etc/hosts
-cat <<EOL > /etc/hosts
-127.0.0.1   localhost
-::1         localhost
-127.0.1.1   $hostname.localdomain $hostname
-EOL
-
-# Generate locales
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-# Configure ZRAM if enabled
-if [ -n "$zram_pkg" ]; then
-  cat <<EOM > /etc/systemd/zram-generator.conf
-[zram0]
-zram-size = ram / 2
-compression-algorithm = zstd
-EOM
-fi
-
-# Install NetworkManager if selected
-if [ -n "$networkmanager_pkg" ]; then
-  pacman -Sy --noconfirm $networkmanager_pkg
-  systemctl enable NetworkManager --root=/mnt
-fi
 EOF
 
 # Set root password
-arch-chroot /mnt passwd root
+echo "[DEBUG] Prompting for root password"
+root_password=$(dialog --stdout --insecure --passwordbox "Enter root password:" 8 40)
+if [ -z "$root_password" ]; then
+  dialog --msgbox "No password entered. Exiting." 5 40
+  clear
+  exit 1
+fi
+arch-chroot /mnt /bin/bash -c "echo -e '$root_password
+$root_password' | passwd"
 
 # Ask if the user wants to use bash or install zsh
+echo "[DEBUG] Prompting for shell selection"
 dialog --yesno "Would you like to use Zsh as your default shell instead of Bash?" 7 50
 if [ $? -eq 0 ]; then
   arch-chroot /mnt pacman -Sy --noconfirm zsh
@@ -307,18 +310,20 @@ if [ $? -eq 0 ]; then
 fi
 
 # Install rEFInd bootloader with Btrfs support and tweaks
+echo "[DEBUG] Installing rEFInd bootloader"
 dialog --infobox "Installing rEFInd bootloader..." 5 40
 arch-chroot /mnt pacman -Sy --noconfirm refind
 arch-chroot /mnt refind-install
 
 # rEFInd configuration
-# Modify refind.conf
+echo "[DEBUG] Modifying rEFInd configuration"
 arch-chroot /mnt sed -i 's/^#enable_mouse/enable_mouse/' /boot/efi/EFI/refind/refind.conf
 arch-chroot /mnt sed -i 's/^#mouse_speed .*/mouse_speed 8/' /boot/efi/EFI/refind/refind.conf
 arch-chroot /mnt sed -i 's/^#resolution .*/resolution max/' /boot/efi/EFI/refind/refind.conf
 arch-chroot /mnt sed -i 's/^#extra_kernel_version_strings .*/extra_kernel_version_strings linux-hardened,linux-rt-lts,linux-zen,linux-lts,linux-rt,linux/' /boot/efi/EFI/refind/refind.conf
 
 # Create refind_linux.conf with the specified options
+echo "[DEBUG] Creating refind_linux.conf"
 partuuid=$(blkid -s PARTUUID -o value $root_partition)
 initrd_line=""
 if [ -n "$microcode_img" ]; then
@@ -332,9 +337,11 @@ cat << EOF > /mnt/boot/refind_linux.conf
 EOF
 
 # Copy refind_linux.conf to rEFInd directory
+echo "[DEBUG] Copying refind_linux.conf to rEFInd directory"
 cp /mnt/boot/refind_linux.conf /mnt/boot/efi/EFI/refind/
 
 # Create first_login.sh script for initial login instructions
+echo "[DEBUG] Creating first_login.sh script"
 cat << 'EOM' > /mnt/root/first_login.sh
 #!/bin/bash
 
@@ -379,8 +386,10 @@ chmod +x /mnt/root/first_login.sh
 # Add the script to root's .bash_profile
 echo "if [ -f ~/first_login.sh ]; then ~/first_login.sh; fi" >> /mnt/root/.bash_profile
 
-# Finish
-dialog --yesno "Installation complete! Would you like to reboot now or drop to the terminal for additional configuration?\n\nSelect 'No' to drop to the terminal." 10 70
+# Finish installation
+dialog --yesno "Installation complete! Would you like to reboot now or drop to the terminal for additional configuration?
+
+Select 'No' to drop to the terminal." 10 70
 if [ $? -eq 0 ]; then
   # Reboot the system
   umount -R /mnt
